d4f68c9f3c7336a8df13c039e50b236762907e1f
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-15 00:06:58 +0100

    Add app action types

---
+++ src/store/actions/actions.ts
@@ -0,0 +1,21 @@
+import {
+  CharactersInterface,
+  PlanetInterface,
+} from "../../types/app-interfaces";
+
+export type AppActions = PlanetActions | CharacterActions;
+
+export enum ActionTypes {
+  INITIALIZE_PLANETS = "initializePlanets",
+  REMOVE_PLANET = "removePlanet",
+  INITIALIZE_CHARACTERS = "initializeCharacters",
+  REMOVE_CHARACTER = "removeCharacter",
+}
+
+export type PlanetActions =
+  | { type: ActionTypes.INITIALIZE_PLANETS; payload: PlanetInterface[] }
+  | { type: ActionTypes.REMOVE_PLANET; payload: PlanetInterface["id"] };
+
+export type CharacterActions =
+  | { type: ActionTypes.INITIALIZE_CHARACTERS; payload: CharactersInterface[] }
+  | { type: ActionTypes.REMOVE_CHARACTER; payload: CharactersInterface["id"] };
