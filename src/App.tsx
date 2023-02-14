14894bb8fe38e36a8c1b49c721a795817aa79713
Ángel <angelmonclova@gmail.com>
2023-02-14 18:33:28 +0100

    Add component Footer to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
-  return <main className="container"></main>;
+  return (
+    <main className="container">
+      <Footer />
+    </main>
+  );
 }
 
 export default App;
