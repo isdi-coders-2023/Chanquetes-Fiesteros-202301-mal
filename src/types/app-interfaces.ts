24d233f4d4914b0030439c33883230a9a945b525
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-15 00:07:52 +0100

    Add app interfaces for store and data

---
+++ src/types/app-interfaces.ts
@@ -0,0 +1,25 @@
+export interface PlanetInterface {
+  id: number;
+  name: string;
+  type: string;
+  dimension: string;
+  created: string;
+  residents: string[];
+}
+
+export interface CharactersInterface {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  gender: string;
+  origin: {
+    name: string;
+  };
+  location: {
+    name: string;
+  };
+  image: string;
+  url: string;
+  created: string;
+}
