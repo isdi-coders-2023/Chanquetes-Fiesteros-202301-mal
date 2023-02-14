065b66723ad0115d80a8f01452e1b1d93b81a0cd
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-14 16:52:34 +0100

    Add header component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Header from "./components/Header/Header";
 import "./App.css";
 
 function App() {
-  return <main className="container"></main>;
+  return (
+    <main className="container">
+      <Header />
+    </main>
+  );
 }
 
 export default App;
