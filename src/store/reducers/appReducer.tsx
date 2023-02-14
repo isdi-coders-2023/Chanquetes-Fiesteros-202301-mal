a28d7f5ef42c187c2a17058b2ab2df870dabd1e1
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-15 00:07:18 +0100

    Add app reducers

---
+++ src/store/reducers/appReducer.tsx
@@ -0,0 +1,29 @@
+import { ActionTypes, AppActions } from "../actions/actions";
+import { AppState } from "../contexts/app.provider";
+
+const appReducer = (state: AppState, action: AppActions): AppState => {
+  const { planets, characters } = state;
+
+  switch (action.type) {
+    case ActionTypes.INITIALIZE_PLANETS:
+      return { ...state, planets: action.payload };
+    case ActionTypes.REMOVE_PLANET:
+      const newPlanets = planets.filter(
+        (planet) => planet.id !== action.payload
+      );
+      return { ...state, planets: newPlanets };
+
+    case ActionTypes.INITIALIZE_CHARACTERS:
+      return { ...state, characters: action.payload };
+    case ActionTypes.REMOVE_CHARACTER:
+      const newCharacters = characters.filter(
+        (character) => character.id !== action.payload
+      );
+      return { ...state, characters: newCharacters };
+
+    default:
+      return state;
+  }
+};
+
+export default appReducer;
