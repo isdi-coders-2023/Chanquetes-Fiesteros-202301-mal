bf4bb7dca30c8a13ff36c6c7ccfafee6614da523
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-15 15:37:55 +0100

    Add necessary changes to pass netlify deploy

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { Footer } from "./Footer";
+import Footer from "./Footer";
 describe("Given a Footer component", () => {
   test("When the footer render, must be render in the documment and check paragraph DEvelopers GitHub", () => {
     render(<Footer />);
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -60,4 +60,4 @@ const Footer = () => {
   );
 };
 
-export { Footer };
+export default Footer;
