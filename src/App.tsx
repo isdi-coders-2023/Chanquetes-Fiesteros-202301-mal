9f057c9bbe47f76e507883148defa88e463a8bd5
Paco Leyva <118618592+Fleyvad@users.noreply.github.com>
2023-02-15 21:09:36 +0100

    Erase link contact component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header/Header";
 import "./App.css";
 import { Footer } from "./components/Footer/Footer";
-
 function App() {
   return (
     <main className="container">
