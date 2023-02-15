9e740abe0071e86037133adfe2989a7b56bd0b19
Ángel <angelmonclova@gmail.com>
2023-02-15 20:41:08 +0100

    Modify App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import "./App.css";
-import { Footer } from "./components/Footer/Footer";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Card/Card";
 
 function App() {
   return (
     <main className="container">
       <Header />
+      <Home />
       <Footer />
     </main>
   );
