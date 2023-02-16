43eade5cfeeafe07a4cca8ca3c8b9f8c4857e041
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-16 09:30:29 +0100

    Fix footer test not working

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,8 +1,10 @@
+import { MemoryRouter } from "react-router-dom";
+
 import { render, screen } from "@testing-library/react";
 import Footer from "./Footer";
 describe("Given a Footer component", () => {
   test("When the footer render, must be render in the documment and check paragraph DEvelopers GitHub", () => {
-    render(<Footer />);
+    render(<Footer />, { wrapper: MemoryRouter });
     const paragraph = screen.getByRole("paragraph");
     expect(paragraph).toHaveTextContent("Developers Github");
   });
