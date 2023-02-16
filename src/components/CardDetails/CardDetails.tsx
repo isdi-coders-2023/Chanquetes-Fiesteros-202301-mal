8659d3873aa19883f015a2a81708baa738772b5d
Paco Leyva <118618592+Fleyvad@users.noreply.github.com>
2023-02-16 16:34:56 +0100

    Create CardDetails component folder with empty files

---
+++ src/components/CardDetails/CardDetails.tsx
@@ -0,0 +1,19 @@
+import "../CardDetails/CardDetails.css";
+
+const CardDetails = () => {
+  return (
+    <div className="">
+      <img src="./1.jpeg" alt="" />
+      <h1>Rick Sanchez</h1>
+      <ul>
+        <li>Status:</li>
+        <li>Specis:</li>
+        <li>Type:</li>
+        <li>Gender:</li>
+        <li>Origin:</li>
+      </ul>
+    </div>
+  );
+};
+
+export default CardDetails;
