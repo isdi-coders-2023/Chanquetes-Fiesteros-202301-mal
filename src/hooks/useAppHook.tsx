c468521a07836b074a8ca2a079150383401665f5
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-17 01:28:17 +0100

    Change app hook to prevent api calls loop

--- src/hooks/useAppHook.tsx
+++ src/hooks/useAppHook.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { getCharactersData, getPlanetsData } from "../api/data";
 import { ActionTypes, AppActions } from "../store/actions/actions";
 import AppContext from "../store/contexts/app.context";
@@ -6,23 +6,29 @@ import AppContext from "../store/contexts/app.context";
 const useAppHook = () => {
   const { state, dispatch } = useContext(AppContext);
 
-  const getPlanetsList = async (planets: number[]) => {
-    const planetsData = await getPlanetsData(planets);
-    const initializePlanetsAction = {
-      type: ActionTypes.INITIALIZE_PLANETS,
-      payload: planetsData,
-    };
-    dispatch(initializePlanetsAction as AppActions);
-  };
+  const getPlanetsList = useCallback(
+    async (planets: number[]) => {
+      const planetsData = await getPlanetsData(planets);
+      const initializePlanetsAction: AppActions = {
+        type: ActionTypes.INITIALIZE_PLANETS,
+        payload: planetsData,
+      };
+      dispatch(initializePlanetsAction);
+    },
+    [dispatch]
+  );
 
-  const getCharactersList = async (characters: number[]) => {
-    const charactersData = await getCharactersData(characters);
-    const initializeCharactersAction = {
-      type: ActionTypes.INITIALIZE_CHARACTERS,
-      payload: charactersData,
-    };
-    dispatch(initializeCharactersAction as AppActions);
-  };
+  const getCharactersList = useCallback(
+    async (characters: number[]) => {
+      const charactersData = await getCharactersData(characters);
+      const initializeCharactersAction: AppActions = {
+        type: ActionTypes.INITIALIZE_CHARACTERS,
+        payload: charactersData,
+      };
+      dispatch(initializeCharactersAction);
+    },
+    [dispatch]
+  );
 
   return { state, getPlanetsList, getCharactersList };
 };
