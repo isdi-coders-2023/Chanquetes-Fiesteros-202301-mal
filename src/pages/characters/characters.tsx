08199ba9bcbf7baa2a0646536deb950c6f20c6e8
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-18 00:35:49 +0100

    Commit to fix netlify bug not reading file changes (3rd try)

--- src/pages/characters/characters.tsx
+++ src/pages/characters/characters.tsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+import CharacterList from "../../components/CharacterList/CharacterList";
+import useAppHook from "../../hooks/useAppHook";
+
 const Characters = () => {
-  return <></>;
+  const { state, getCharactersList } = useAppHook();
+
+  useEffect(() => {
+    getCharactersList();
+  }, [getCharactersList]);
+
+  return (
+    <>
+      <CharacterList characters={state.characters} />
+    </>
+  );
 };
 
 export default Characters;
