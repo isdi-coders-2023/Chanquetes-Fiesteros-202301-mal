b5bc3a7c38f64a862ed604ddfbc072aee4bf42fc
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-17 09:14:21 +0100

    Change router imports for github workflow

--- src/router/app-router.tsx
+++ src/router/app-router.tsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "../pages/layout/layout";
+import Layout from "../pages/Layout/Layout";
 import NotFound from "../pages/not-found/not-found";
-import Home from "../pages/home/home";
-import Contact from "../pages/contact/contact";
-import Favorites from "../pages/favorites/favorites";
 import PlanetDetails from "../pages/planet-details/planet-details";
 
 const router = createBrowserRouter([
@@ -12,18 +9,6 @@ const router = createBrowserRouter([
     element: <Layout />,
     errorElement: <NotFound />,
     children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-      {
-        path: "favorites",
-        element: <Favorites />,
-      },
       {
         path: "planet-details",
         element: <PlanetDetails />,
