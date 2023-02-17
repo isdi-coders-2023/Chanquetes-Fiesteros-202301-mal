580ecf1f21075450fc32a10f90b1f2c2ca453132
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-18 00:17:03 +0100

    Fix mock handlers (warnings on uncaught calls removed)

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,7 +2,7 @@
 import { rest } from "msw";
 
 export const handlers = [
-  rest.get(`https://rickandmortyapi.com/api/location/1`, (_req, res, ctx) => {
+  rest.get("https://rickandmortyapi.com/api/location/1", (_req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
@@ -11,12 +11,39 @@ export const handlers = [
       })
     );
   }),
-  rest.get(`https://rickandmortyapi.com/api/character/1`, (_req, res, ctx) => {
+  rest.get("https://rickandmortyapi.com/api/character/1", (_req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
         id: 1,
         name: "Rick Sanchez",
+      })
+    );
+  }),
+
+  rest.get("https://rickandmortyapi.com/api/location", (_req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        results: [
+          {
+            id: 1,
+            name: "Rick Sanchez",
+          },
+        ],
+      })
+    );
+  }),
+  rest.get("https://rickandmortyapi.com/api/character", (_req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        results: [
+          {
+            id: 1,
+            name: "Rick Sanchez",
+          },
+        ],
       })
     );
   }),
