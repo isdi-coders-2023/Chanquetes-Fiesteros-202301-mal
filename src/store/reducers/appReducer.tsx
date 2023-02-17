0dbfdcf34c13e1a170f806cdc7a847a6b1015744
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-17 01:23:56 +0100

    Add reducer cases for pagination feature

--- src/store/reducers/appReducer.tsx
+++ src/store/reducers/appReducer.tsx
@@ -1,25 +1,46 @@
 import { ActionTypes, AppActions } from "../actions/actions";
-import { AppState } from "../../types/appInterfaces";
+import {
+  AppState,
+  CharactersInterface,
+  PlanetInterface,
+} from "../../types/appInterfaces";
 
 const appReducer = (state: AppState, action: AppActions): AppState => {
-  const { planets, characters } = state;
+  const { pagination } = state;
+
+  let allPlanets: PlanetInterface[] = [];
+  let allCharacters: CharactersInterface[] = [];
 
   switch (action.type) {
+    // Plantes actions
     case ActionTypes.INITIALIZE_PLANETS:
-      return { ...state, planets: action.payload };
+      allPlanets = action.payload;
+      return { ...state, planets: allPlanets };
     case ActionTypes.REMOVE_PLANET:
-      const newPlanets = planets.filter(
+      const newPlanets = allPlanets.filter(
         (planet) => planet.id !== action.payload
       );
       return { ...state, planets: newPlanets };
 
+    // Characters actions
     case ActionTypes.INITIALIZE_CHARACTERS:
-      return { ...state, characters: action.payload };
+      allCharacters = action.payload;
+      return { ...state, characters: allCharacters };
     case ActionTypes.REMOVE_CHARACTER:
-      const newCharacters = characters.filter(
+      const newCharacters = allCharacters.filter(
         (character) => character.id !== action.payload
       );
       return { ...state, characters: newCharacters };
+
+    // Pagination actions
+    case ActionTypes.GO_TO_NEXT_PAGE:
+      return pagination < action.payload
+        ? { ...state, pagination: pagination + 1 }
+        : { ...state, pagination: pagination };
+    case ActionTypes.GO_TO_PREV_PAGE:
+      return pagination > 1
+        ? { ...state, pagination: pagination - 1 }
+        : { ...state, pagination: pagination };
 
     default:
       return state;
