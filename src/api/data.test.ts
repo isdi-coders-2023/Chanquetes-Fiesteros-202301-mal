d5d57f509d004a553f9fba74c20d6e896fb99093
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-18 23:16:56 +0100

    Add missing tests for the new data functions

--- src/api/data.test.ts
+++ src/api/data.test.ts
@@ -1,6 +1,11 @@
 import { server } from "../mocks/server";
 import { CharacterInterface, PlanetInterface } from "../types/appInterfaces";
-import { getPlanetsData, getCharactersData } from "./data";
+import {
+  getPlanetsData,
+  getCharactersData,
+  getPlanetById,
+  getCharacterById,
+} from "./data";
 
 describe("Given an API call", () => {
   beforeAll(() => server.listen());
@@ -14,5 +19,15 @@ describe("Given an API call", () => {
   test("When the server respond with a 200, then the expected array should have characters data", async () => {
     const charactersList: CharacterInterface[] = await getCharactersData(1);
     expect(charactersList[0].id).toBe(1);
+  });
+
+  test("When the server respond with a 200, then the expected array should have the specified planet data", async () => {
+    const planetData: PlanetInterface = await getPlanetById(1);
+    expect(planetData.id).toBe(1);
+  });
+
+  test("When the server respond with a 200, then the expected array should have the specified character data", async () => {
+    const characterData: CharacterInterface = await getCharacterById(1);
+    expect(characterData.id).toBe(1);
   });
 });
