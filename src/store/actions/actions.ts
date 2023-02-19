5e6e73deba5fb578e76de44bd65ddaf874c1e8ff
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-19 21:24:03 +0100

    Add fav character needed actions

--- src/store/actions/actions.ts
+++ src/store/actions/actions.ts
@@ -1,6 +1,6 @@
-import { CharacterInterface, PlanetInterface } from "../../types/appInterfaces";
+import { Character, FavCharacter, Planet } from "../../types/appInterfaces";
 
-export type AppActions = PlanetActions | CharacterActions;
+export type AppActions = PlanetActions | CharacterActions | FavoriteActions;
 
 export enum ActionTypes {
   GET_PLANETS = "getPlanets",
@@ -13,18 +13,45 @@ export enum ActionTypes {
   PREV_PAGE_CHARACTERS = "prevPageCharacters",
   GET_PLANET_DETAILS = "getPlanetDetails",
   GET_CHARACTER_DETAILS = "getCharacterDetails",
+  GET_FAV_CHARACTERS = "getFavCharacters",
+  ADD_FAV_CHARACTER = "addFavCharacter",
+  CHANGE_FAV_CHARACTER_NAME = "changeFavCharacterName",
+  SET_FAV_CHARACTER_RATING = "setFavCharacterRating",
+  REMOVE_FAV_CHARACTER = "removeFavCharacter",
 }
 
 export type PlanetActions =
-  | { type: ActionTypes.GET_PLANETS; payload: PlanetInterface[] }
-  | { type: ActionTypes.REMOVE_PLANET; payload: PlanetInterface["id"] }
+  | { type: ActionTypes.GET_PLANETS; payload: Planet[] }
+  | { type: ActionTypes.REMOVE_PLANET; payload: Planet["id"] }
   | { type: ActionTypes.NEXT_PAGE_PLANETS; payload: number }
   | { type: ActionTypes.PREV_PAGE_PLANETS }
-  | { type: ActionTypes.GET_PLANET_DETAILS; payload: PlanetInterface };
+  | { type: ActionTypes.GET_PLANET_DETAILS; payload: Planet };
 
 export type CharacterActions =
-  | { type: ActionTypes.GET_CHARACTERS; payload: CharacterInterface[] }
-  | { type: ActionTypes.REMOVE_CHARACTER; payload: CharacterInterface["id"] }
+  | { type: ActionTypes.GET_CHARACTERS; payload: Character[] }
+  | { type: ActionTypes.REMOVE_CHARACTER; payload: Character["id"] }
   | { type: ActionTypes.NEXT_PAGE_CHARACTERS; payload: number }
   | { type: ActionTypes.PREV_PAGE_CHARACTERS }
-  | { type: ActionTypes.GET_CHARACTER_DETAILS; payload: CharacterInterface };
+  | { type: ActionTypes.GET_CHARACTER_DETAILS; payload: Character };
+
+export type FavoriteActions =
+  | {
+      type: ActionTypes.GET_FAV_CHARACTERS;
+      payload: FavCharacter[];
+    }
+  | {
+      type: ActionTypes.ADD_FAV_CHARACTER;
+      payload: FavCharacter;
+    }
+  | {
+      type: ActionTypes.CHANGE_FAV_CHARACTER_NAME;
+      payload: FavCharacter["name"];
+    }
+  | {
+      type: ActionTypes.SET_FAV_CHARACTER_RATING;
+      payload: FavCharacter["rating"];
+    }
+  | {
+      type: ActionTypes.REMOVE_FAV_CHARACTER;
+      payload: FavCharacter["id"];
+    };
