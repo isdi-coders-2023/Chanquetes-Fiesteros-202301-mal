02be9d5669d462c8e01303826923c9601e2cd3a1
JoaccoG <joaquingodoy2407@gmail.com>
2023-02-20 00:37:52 +0100

    Remove unnecessary actions and cases from reducer

--- src/store/actions/actions.ts
+++ src/store/actions/actions.ts
@@ -4,53 +4,31 @@ export type AppActions = PlanetActions | CharacterActions | FavoriteActions;
 
 export enum ActionTypes {
   GET_PLANETS = "getPlanets",
-  REMOVE_PLANET = "removePlanet",
+  GET_PLANET_DETAILS = "getPlanetDetails",
   NEXT_PAGE_PLANETS = "nextPagePlanets",
   PREV_PAGE_PLANETS = "prevPagePlanets",
+
   GET_CHARACTERS = "getCharacters",
-  REMOVE_CHARACTER = "removeCharacter",
+  GET_CHARACTER_DETAILS = "getCharacterDetails",
   NEXT_PAGE_CHARACTERS = "nextPageCharacters",
   PREV_PAGE_CHARACTERS = "prevPageCharacters",
-  GET_PLANET_DETAILS = "getPlanetDetails",
-  GET_CHARACTER_DETAILS = "getCharacterDetails",
+
   GET_FAV_CHARACTERS = "getFavCharacters",
-  ADD_FAV_CHARACTER = "addFavCharacter",
-  CHANGE_FAV_CHARACTER_NAME = "changeFavCharacterName",
-  SET_FAV_CHARACTER_RATING = "setFavCharacterRating",
-  REMOVE_FAV_CHARACTER = "removeFavCharacter",
+  UPDATE_FAV_CHARACTERS = "updateFavCharacters",
 }
 
 export type PlanetActions =
   | { type: ActionTypes.GET_PLANETS; payload: Planet[] }
-  | { type: ActionTypes.REMOVE_PLANET; payload: Planet["id"] }
+  | { type: ActionTypes.GET_PLANET_DETAILS; payload: Planet }
   | { type: ActionTypes.NEXT_PAGE_PLANETS; payload: number }
-  | { type: ActionTypes.PREV_PAGE_PLANETS }
-  | { type: ActionTypes.GET_PLANET_DETAILS; payload: Planet };
+  | { type: ActionTypes.PREV_PAGE_PLANETS };
 
 export type CharacterActions =
   | { type: ActionTypes.GET_CHARACTERS; payload: Character[] }
-  | { type: ActionTypes.REMOVE_CHARACTER; payload: Character["id"] }
+  | { type: ActionTypes.GET_CHARACTER_DETAILS; payload: Character }
   | { type: ActionTypes.NEXT_PAGE_CHARACTERS; payload: number }
-  | { type: ActionTypes.PREV_PAGE_CHARACTERS }
-  | { type: ActionTypes.GET_CHARACTER_DETAILS; payload: Character };
+  | { type: ActionTypes.PREV_PAGE_CHARACTERS };
 
 export type FavoriteActions =
-  | {
-      type: ActionTypes.GET_FAV_CHARACTERS;
-    }
-  | {
-      type: ActionTypes.ADD_FAV_CHARACTER;
-      payload: FavCharacter;
-    }
-  | {
-      type: ActionTypes.CHANGE_FAV_CHARACTER_NAME;
-      payload: [FavCharacter["id"], FavCharacter["name"]];
-    }
-  | {
-      type: ActionTypes.SET_FAV_CHARACTER_RATING;
-      payload: [FavCharacter["id"], FavCharacter["rating"]];
-    }
-  | {
-      type: ActionTypes.REMOVE_FAV_CHARACTER;
-      payload: FavCharacter["id"];
-    };
+  | { type: ActionTypes.GET_FAV_CHARACTERS }
+  | { type: ActionTypes.UPDATE_FAV_CHARACTERS; payload: FavCharacter[] };
--- src/store/reducers/appReducer.tsx
+++ src/store/reducers/appReducer.tsx
@@ -3,8 +3,6 @@ import { AppState } from "../../types/appInterfaces";
 
 const appReducer = (state: AppState, action: AppActions): AppState => {
   const {
-    planets,
-    characters,
     currentPagePlanets: planetsPages,
     currentPageCharacters: charactersPages,
     favCharacters,
@@ -14,11 +12,8 @@ const appReducer = (state: AppState, action: AppActions): AppState => {
     // Planets actions
     case ActionTypes.GET_PLANETS:
       return { ...state, planets: action.payload };
-    case ActionTypes.REMOVE_PLANET:
-      const newPlanets = planets.filter(
-        (planet) => planet.id !== action.payload
-      );
-      return { ...state, planets: newPlanets };
+    case ActionTypes.GET_PLANET_DETAILS:
+      return { ...state, planetDetails: action.payload };
     case ActionTypes.NEXT_PAGE_PLANETS:
       return planetsPages < action.payload
         ? { ...state, currentPagePlanets: planetsPages + 1 }
@@ -27,17 +22,12 @@ const appReducer = (state: AppState, action: AppActions): AppState => {
       return planetsPages > 1
         ? { ...state, currentPagePlanets: planetsPages - 1 }
         : { ...state, currentPagePlanets: planetsPages };
-    case ActionTypes.GET_PLANET_DETAILS:
-      return { ...state, planetDetails: action.payload };
 
     // Characters actions
     case ActionTypes.GET_CHARACTERS:
       return { ...state, characters: action.payload };
-    case ActionTypes.REMOVE_CHARACTER:
-      const newCharacters = characters.filter(
-        (character) => character.id !== action.payload
-      );
-      return { ...state, characters: newCharacters };
+    case ActionTypes.GET_CHARACTER_DETAILS:
+      return { ...state, characterDetails: action.payload };
     case ActionTypes.NEXT_PAGE_CHARACTERS:
       return charactersPages < action.payload
         ? { ...state, currentPageCharacters: charactersPages + 1 }
@@ -46,35 +36,12 @@ const appReducer = (state: AppState, action: AppActions): AppState => {
       return charactersPages > 1
         ? { ...state, currentPageCharacters: charactersPages - 1 }
         : { ...state, currentPageCharacters: charactersPages };
-    case ActionTypes.GET_CHARACTER_DETAILS:
-      return { ...state, characterDetails: action.payload };
 
     // Favorite characters actions
     case ActionTypes.GET_FAV_CHARACTERS:
       return { ...state, ...favCharacters };
-    case ActionTypes.ADD_FAV_CHARACTER:
-      return { ...state, favCharacters: [...favCharacters, action.payload] };
-    case ActionTypes.CHANGE_FAV_CHARACTER_NAME:
-      const updatedFavCharactersName = favCharacters.map((character) => {
-        if (character.id === action.payload[0]) {
-          return { ...character, name: action.payload[1] };
-        }
-        return character;
-      });
-      return { ...state, favCharacters: updatedFavCharactersName };
-    case ActionTypes.SET_FAV_CHARACTER_RATING:
-      const updatedFavCharactersRating = favCharacters.map((character) => {
-        if (character.id === action.payload[0]) {
-          return { ...character, rating: action.payload[1] };
-        }
-        return character;
-      });
-      return { ...state, favCharacters: updatedFavCharactersRating };
-    case ActionTypes.REMOVE_FAV_CHARACTER:
-      const updatedFavCharacters = favCharacters.filter(
-        (character) => character.id !== action.payload
-      );
-      return { ...state, favCharacters: updatedFavCharacters };
+    case ActionTypes.UPDATE_FAV_CHARACTERS:
+      return { ...state, favCharacters: action.payload };
 
     default:
       return state;
